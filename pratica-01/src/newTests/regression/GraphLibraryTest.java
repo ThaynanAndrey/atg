f1be550715a9e994a5f4a3872757e6a88eb7e45d
Matheus Freitas <matheusbfreitas@gmail.com>
2018-06-06 23:34:07 -0300

    retira testes de bfs da library

--- pratica-01/src/newTests/regression/GraphLibraryTest.java
+++ pratica-01/src/newTests/regression/GraphLibraryTest.java
@@ -1,6 +1,5 @@
 package newTests.regression;
 
-import graph.Edge;
 import graph.Graph;
 import library.GraphLibrary;
 import newTests.utils.UtilsTest;
@@ -11,8 +10,8 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.Set;
 
-import static java.lang.Character.LINE_SEPARATOR;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Class to test {@link GraphLibrary}.
@@ -22,16 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GraphLibraryTest {
 
     GraphLibrary graphLibrary;
-    private Graph regularGraph;
-    private Graph completeGraph;
-    private Graph disconnectedGraph;
-    private Edge edge12 = new Edge(1,2);
-    private Edge edge13 = new Edge(1,3);
-    private Edge edge24 = new Edge(2,4);
-    private Edge edge34 = new Edge(3,4);
-    private Edge edge14 = new Edge(1,4);
-    private Edge edge23 = new Edge(2,3);
-    private Edge edge56 = new Edge(5,6);
+
 
     /**
      * Tests set up.
@@ -39,9 +29,6 @@ public class GraphLibraryTest {
     @BeforeEach
     public void setUp() {
         graphLibrary = new GraphLibrary();
-        setUpCompletegraph();
-        setUpDisconectedGraph();
-        setUpRegularGraph();
     }
 
     /**
@@ -53,59 +40,6 @@ public class GraphLibraryTest {
         UtilsTest.deleteFile("weighted_graph.txt");
     }
 
-    private void setUpRegularGraph(){
-        regularGraph = new Graph();
-        regularGraph.addEdge(1,edge12);
-        regularGraph.addEdge(2,edge12);
-
-        regularGraph.addEdge(1,edge13);
-        regularGraph.addEdge(3,edge13);
-
-        regularGraph.addEdge(2,edge24);
-        regularGraph.addEdge(4,edge24);
-
-        regularGraph.addEdge(3,edge34);
-        regularGraph.addEdge(4,edge34);
-    }
-
-    private void setUpCompletegraph(){
-        completeGraph = new Graph();
-        completeGraph.addEdge(1,edge12);
-        completeGraph.addEdge(2,edge12);
-
-        completeGraph.addEdge(1,edge13);
-        completeGraph.addEdge(3,edge13);
-
-        completeGraph.addEdge(2,edge24);
-        completeGraph.addEdge(4,edge24);
-
-        completeGraph.addEdge(3,edge34);
-        completeGraph.addEdge(4,edge34);
-
-        completeGraph.addEdge(1,edge14);
-        completeGraph.addEdge(4,edge14);
-
-        completeGraph.addEdge(2,edge23);
-        completeGraph.addEdge(3,edge23);
-
-    }
-
-    private void setUpDisconectedGraph(){
-        disconnectedGraph = new Graph();
-        disconnectedGraph.addEdge(1,edge12);
-        disconnectedGraph.addEdge(2,edge12);
-
-        disconnectedGraph.addEdge(1,edge13);
-        disconnectedGraph.addEdge(3,edge13);
-
-        disconnectedGraph.addEdge(2,edge24);
-        disconnectedGraph.addEdge(4,edge24);
-
-        disconnectedGraph.addEdge(3,edge34);
-        disconnectedGraph.addEdge(4,edge34);
-
-        disconnectedGraph.addEdge(5,edge56);
-    }
 
     /**
      * Insert file for future tests.
@@ -171,40 +105,5 @@ public class GraphLibraryTest {
         assertTrue(allVertexes.contains(5));
         assertTrue(allVertexes.contains(6));
     }
-
-    @Test
-    public void BFSRegularGraphTest(){
-        String regularBFSFrom1 = new StringBuilder()
-                .append("1 - 0 -").append(LINE_SEPARATOR)
-                .append("2 - 1 1").append(LINE_SEPARATOR)
-                .append("3 - 1 1").append(LINE_SEPARATOR)
-                .append("4 - 2 2").append(LINE_SEPARATOR)
-                .toString();
-
-        assertEquals(graphLibrary.BFS(regularGraph,1), regularBFSFrom1);
-    }
-
-    @Test
-    public void BFSCompleteGraphTest(){
-        String completeBFSFrom1 = new StringBuilder()
-                .append("1 - 0 -").append(LINE_SEPARATOR)
-                .append("2 - 1 1").append(LINE_SEPARATOR)
-                .append("3 - 1 1").append(LINE_SEPARATOR)
-                .append("4 - 1 1").append(LINE_SEPARATOR)
-                .toString();
-
-        assertEquals(graphLibrary.BFS(completeGraph,1), completeBFSFrom1);
-    }
-
-    @Test
-    public void BFSDisconectedGraphTest(){
-        String disconnectedBFSFrom1 = new StringBuilder()
-                .append("1 - 0 -").append(LINE_SEPARATOR)
-                .append("2 - 1 1").append(LINE_SEPARATOR)
-                .append("3 - 1 1").append(LINE_SEPARATOR)
-                .append("4 - 2 2").append(LINE_SEPARATOR)
-                .toString();
-
-        assertEquals(graphLibrary.BFS(disconnectedGraph,1), disconnectedBFSFrom1);
-    }
+    
 }
