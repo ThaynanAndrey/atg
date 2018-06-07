e70ed2ed5cde90c823dcd5e086be8e61ccc6002c
Thaynan Andrey <thaynan.nunes@ccc.ufcg.edu.br>
2018-06-06 21:56:55 -0300

    Adicionados testes para GrapLibrary, GraphCreator, GraphReader, RawGrapah e VertexInfo

--- pratica-01/src/newTests/unit/GraphReaderTest.java
+++ pratica-01/src/newTests/unit/GraphReaderTest.java
@@ -1,6 +1,18 @@
 package newTests.unit;
 
 import graph.GraphReader;
+import graph.RawGraph;
+import newTests.utils.UtilsTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Class to test {@link GraphReader}.
@@ -9,4 +21,98 @@ import graph.GraphReader;
  */
 public class GraphReaderTest {
 
+    /**
+     * Insert file for future tests.
+     *
+     * @param path File's path.
+     * @param fileContent Content to be insert in file.
+     */
+    private void insertFile(String path, String fileContent) {
+        try {
+            UtilsTest.insertFile(path, fileContent);
+        } catch (IOException e) {
+            fail("There was an error on file's writen.");
+        }
+    }
+
+    /**
+     * Deletes the files created for the tests.
+     */
+    @AfterAll
+    public static void deleteFile() {
+        UtilsTest.deleteFile("graph.txt");
+    }
+
+    /**
+     * Test if read file and create correctly a {@link RawGraph}.
+     */
+    @Test
+    public void readTest() {
+        String path = "graph.txt";
+        String fileContent = "4, 1 2, 1 3, 2 3, 4 3";
+        insertFile(path, fileContent);
+
+        RawGraph rawGraph = GraphReader.read(path);
+        List<String> rawGraphList = rawGraph.getRawGraph();
+
+        assertEquals(rawGraph.getVertexNumber(), 4);
+        assertEquals(rawGraph.getNumEdges(), 4);
+        assertTrue(rawGraphList.contains("4"));
+        assertTrue(rawGraphList.contains("1 2"));
+        assertTrue(rawGraphList.contains("1 3"));
+        assertTrue(rawGraphList.contains("2 3"));
+        assertTrue(rawGraphList.contains("4 3"));
+    }
+
+    /**
+     * Test if read file and create a {@link RawGraph} to unweigthed dense graph.
+     */
+    @Test
+    public void readTestUnweightedDenseGrap() {
+        String path = "src/newTests/resources/dense_unweighted_graph_500.txt";
+        RawGraph rawGraph = GraphReader.read(path);
+        List<String> rawGraphList = rawGraph.getRawGraph();
+
+        assertEquals(rawGraph.getVertexNumber(), 500);
+        assertEquals(rawGraph.getNumEdges(), 130570);
+    }
+
+    /**
+     * Test if read file and create a {@link RawGraph} to weigthed dense graph.
+     */
+    @Test
+    public void readTestweightedDenseGrap() {
+        String path = "src/newTests/resources/dense_weighted_graph_500.txt";
+        RawGraph rawGraph = GraphReader.read(path);
+        List<String> rawGraphList = rawGraph.getRawGraph();
+
+        assertEquals(rawGraph.getVertexNumber(), 500);
+        assertEquals(rawGraph.getNumEdges(), 130277);
+    }
+
+    /**
+     * Test if read file and create a {@link RawGraph} to unweigthed sparse graph.
+     */
+    @Test
+    public void readTestUnweightedSparseGrap() {
+        String path = "src/newTests/resources/sparse_unweighted_graph_500.txt";
+        RawGraph rawGraph = GraphReader.read(path);
+        List<String> rawGraphList = rawGraph.getRawGraph();
+
+        assertEquals(rawGraph.getVertexNumber(), 500);
+        assertEquals(rawGraph.getNumEdges(), 17375);
+    }
+
+    /**
+     * Test if read file and create a {@link RawGraph} to weigthed sparse graph.
+     */
+    @Test
+    public void readTestWeightedSparseGrap() {
+        String path = "src/newTests/resources/sparse_weighted_graph_500.txt";
+        RawGraph rawGraph = GraphReader.read(path);
+        List<String> rawGraphList = rawGraph.getRawGraph();
+
+        assertEquals(rawGraph.getVertexNumber(), 500);
+        assertEquals(rawGraph.getNumEdges(), 11766);
+    }
 }
